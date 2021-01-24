2ca8171701c7d9a2c732882f35da240f7c81abbc
Josh Chang <chang529@purdue.edu>
2021-01-24 02:25:32 -0500

    list render

--- src/Form.js
+++ src/Form.js
@@ -7,15 +7,19 @@ constructor(props) {
   super(props)
 
   this.state = {
-      day: '',
-      Comments: '',
-      rate: ''
+
+
+      days: ['great','meh','bad'],
+      Comments: ['it was good','whatever','shit'],
+      rate: []
+    
   }
 }
 
+
   handleDayChange = (event) => {
       this.setState({
-        day: event.target.value
+        days: event.target.value
       })
   }
 
@@ -32,15 +36,27 @@ constructor(props) {
   }
 
   handleSubmit = (event) =>{
-      alert(JSON.stringify(this.state.day) + JSON.stringify(this.state.comments) + JSON.stringify(this.state.rate))
+      alert(JSON.stringify(this.state.days) + JSON.stringify(this.state.comments) + JSON.stringify(this.state.rate))
+      const {days} = this.state;
+      const newday = this.newday.value;
       event.preventDefault()
+
+      this.setState({
+        days: [...this.state.days, newday]
+      })
+
+      this.addForm.reset();
 }
+
+
   render(){
+    //const {days} = this.state;
     return(
-      <form onSubmit={this.handleSubmit}>
+    <div className ="content">
+      <form ref={input => this.addForm = input} onSubmit={this.handleSubmit}>
         <div>
           <label>How was your day? :)</label>
-          <input type='text' value ={this.state.day}  onChange = {this.handleDayChange}/>
+          <input ref={input => this.newday = input} type='text' value ={this.state.days}  onChange = {this.handleDayChange}/>
         </div>
         <div>
           <label>Why?</label>
@@ -56,9 +72,42 @@ constructor(props) {
           <option value= "five">5</option>
           </select>
         </div>
-        <button type="Submit">Submit</button>
+        <button type="submit" className="btn btn-primary"> Submit</button>
       </form>
-      <div>JSON.stringify(this.state.day)</div>
+      <div>
+        <h1>List of days</h1>
+      
+
+        <table className="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">day</th>
+      <th scope="col">comment</th>
+      <th scope="col">rate</th>
+    </tr>
+  </thead>
+  <tbody>
+  {
+            this.state.days.map(day => {
+            return (<tr key={day}>
+              <th scope="row">1</th>
+              <td>{day}</td>
+              <td>test1</td>
+              <td>test2</td>
+            </tr>)
+          })
+        }
+
+  </tbody>
+</table>
+
+
+
+      </div>
+      
+
+    </div>
     )
 
   }
